2a8f93ffd355880abab3fec51fc02cfa8a41fb2a
Donal Johny <cyberbemon@users.noreply.github.com>
2017-07-17 18:36:03 +0100

    A space shooter made in Unity
    First commit of UnitySpaceShooter.
    Finished setting up the camera
    Setting up the game background
    Adding player and scripts for basic player movement.

---
+++ UnitySpaceShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    public float tilt;
+
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb = GetComponent<Rigidbody>();
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.velocity = movement * speed;
+
+        // Clamp our movement, so the player doesn't go out of play area.
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt );
+    }
+}
