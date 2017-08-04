1385ddafb5e9218bc043893eb558bac468db4163
Donal Johny <cyberbemon@users.noreply.github.com>
2017-08-04 16:40:57 +0100

    AStarPathfindingForUnits
    Added units that'll request path to a target.
    Organized the code
    Cleaned up code.
    Added characters and animation instead of using capsules to represent units.

---
+++ AStarPathfindingForUnits/Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class Pathfinding : MonoBehaviour {
+
+    PathRequestManager requestManager;
+    Grid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+        requestManager = GetComponent<PathRequestManager>();
+    }
+
+    public void StartFindPath(Vector3 pathStart, Vector3 pathEnd)
+    {
+        StartCoroutine(FindPath(pathStart, pathEnd));
+    }
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="startPos"></param>
+    /// <param name="targetPos"></param>
+    IEnumerator FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        Vector3[] waypoints = new Vector3[0];
+        bool pathSuccess = false;
+        //  Convert worldpositions to Nodes 
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        // We only want to proceed to find a path if both the nodes are walkable
+        if (startNode.walkable && targetNode.walkable)
+        {
+            Heap<Node> openSet = new Heap<Node>(grid.MaxSize);
+            HashSet<Node> closedSet = new HashSet<Node>();
+            openSet.Add(startNode);
+            /*
+             * openSet - the set of nodes to be evaluated
+             * closedSet  - the set of nodes already evaluated
+             * add the start node to OpenSet
+             * loop
+             *  current = node in openSet with the lowest f_cost
+             *  remove current from openSet
+             *  add current to closedSet
+             *  if current is the target node - path has been found 
+             *      return
+             *  foreach neighbour of the current node
+             *      if neighbour is not traversable or neighbour is in closedSet
+             *          skip to the next neighbour
+             *      
+             *      if new path to neighbour is shorter or neighbour is not in openSet
+             *          set f_cost of neighbour
+             *          set parent of neighbour to current
+             *          if neighbour is not in openSet
+             *              add neighbour to openSet
+             *           
+             */
+            while (openSet.Count > 0)
+            {
+                Node currnetNode = openSet.RemoveFirst();
+
+                closedSet.Add(currnetNode);
+
+                if (currnetNode == targetNode)
+                {
+                    sw.Stop();
+                    print("Path Found: " + sw.ElapsedMilliseconds + "ms");
+                    pathSuccess = true;
+                    break;
+                }
+
+                foreach (Node neighbour in grid.GetNeighbours(currnetNode))
+                {
+                    if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    int newMovementCostToNeighbour = currnetNode.gCost + getDistance(currnetNode, neighbour);
+                    if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                    {
+                        neighbour.gCost = newMovementCostToNeighbour;
+                        neighbour.hCost = getDistance(neighbour, targetNode);
+                        neighbour.parent = currnetNode;
+
+                        if (!openSet.Contains(neighbour))
+                        {
+                            openSet.Add(neighbour);
+                        }
+                        else
+                        {
+                            openSet.UpdateItem(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+        yield return null;
+        if(pathSuccess)
+        {
+            waypoints = ReTracePath(startNode, targetNode);
+        }
+        requestManager.FinishedProcessingPath(waypoints, pathSuccess);
+    }
+
+    Vector3[] ReTracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currnetNode = endNode;
+
+        while(currnetNode != startNode)
+        {
+            path.Add(currnetNode);
+            currnetNode = currnetNode.parent;
+        }
+        Vector3[] waypoints = SimplifyPath(path);
+        Array.Reverse(waypoints);
+        return waypoints;
+    }
+
+    Vector3[] SimplifyPath(List<Node> path)
+    {
+        List<Vector3> waypoints = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
+        for (int i = 1; i < path.Count; i ++)
+        {
+            Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+            if(directionNew != directionOld)
+            {
+                waypoints.Add(path[i].worldPosition);
+            }
+            directionOld = directionNew;
+        }
+        return waypoints.ToArray();
+    }
+
+    int getDistance(Node nodeA, Node nodeB)
+    {
+        int distanceX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distanceY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (distanceX > distanceY)
+        {
+            return 14 * distanceY + 10 * (distanceX - distanceY);
+        }
+        else
+        {
+            return 14 * distanceX + 10 * (distanceY- distanceX);
+        }
+    }
+}
